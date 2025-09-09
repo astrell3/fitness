908b09bd3ea1bc6064741c11eaac2e2fcd2c0040
Astrell <astr3ll@yandex.ru>
2025-09-09 19:17:37 +0300

    third commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,6 +2,7 @@ package daysteps
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -25,6 +26,10 @@ func parsePackage(data string) (int, time.Duration, error) {
 
 	stepsStr := strings.TrimSpace(parts[0])
 	durationStr := strings.TrimSpace(parts[1])
+
+	if strings.Contains(stepsStr, " ") {
+		return 0, 0, fmt.Errorf("неверный формат шагов")
+	}
 
 	steps, err := strconv.Atoi(stepsStr)
 	if err != nil {
@@ -51,10 +56,12 @@ func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
 	steps, duration, err := parsePackage(data)
 	if err != nil {
+		log.Println(err)
 		return ""
 	}
 
 	if steps <= 0 {
+		log.Println("количество шагов должно быть положительным")
 		return ""
 	}
 
